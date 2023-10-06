288d9776402f0e095b3bc8e92806caf0cf242583
Rasheed Olaoluwa <radeyola@gmail.com>
2023-10-06 13:17:37 -0700

    remove comma

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,7 +15,7 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	printf("%d, ", n);
+	printf("%d ", n);
 	if (n > 0)
 		printf("is positive\n");
 	else if (n == 0)

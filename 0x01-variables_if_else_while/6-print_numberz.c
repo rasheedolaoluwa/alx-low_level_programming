212ec2b33bc6b35e9152835a9bd99f2842a8a8e2
Rasheed Olaoluwa <radeyola@gmail.com>
2023-10-06 14:14:46 -0700

    update code

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -9,12 +9,12 @@ int main(void)
 {
 	int number;
 
-	for (number = 0; number < 10; number++)
+	for (number = '0'; number <= '9'; number++)
 	{
-		printf("%d", number);
+		putchar(number);
 	}
 
-	printf("\n");
+	putchar('\n');
 
 	return (0);
 }

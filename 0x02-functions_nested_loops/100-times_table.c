2df3c576f0d8d32da788cc9fddd4a73e8067d0f5
Rasheed Olaoluwa <radeyola@gmail.com>
2023-10-10 14:10:19 -0700

    betty

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,26 +7,26 @@
  */
 void print_times_table(int n)
 {
-    int row, col, product;
+	int row, col, product;
 
-    if (n >= 0 && n <= 15)
-    {
-        for (row = 0; row <= n; row++)
-        {
-            for (col = 0; col <= n; col++)
-            {
-                product = row * col;
-                if (col != 0)
-                {
-                    printf(", ");
-                    if (product < 10)
-                        printf("  ");
-                    else if (product < 100)
-                        printf(" ");
-                }
-                printf("%d", product);
-            }
-            printf("\n");
-        }
-    }
+	if (n >= 0 && n <= 15)
+	{
+		for (row = 0; row <= n; row++)
+		{
+			for (col = 0; col <= n; col++)
+			{
+				product = row * col;
+				if (col != 0)
+				{
+					printf(", ");
+					if (product < 10)
+						printf("  ");
+					else if (product < 100)
+						printf(" ");
+				}
+				printf("%d", product);
+			}
+			printf("\n");
+		}
+	}
 }

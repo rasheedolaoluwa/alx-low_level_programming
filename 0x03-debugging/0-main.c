ce4c281339f9bef5e791ba7a23d1856f1ddaf496
Rasheed Olaoluwa <radeyola@gmail.com>
2023-10-12 05:18:54 -0700

    main

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+
+int main(void)
+{
+        int i;
+
+        i = 0;  /* Only line changed from 98 to 0 */
+        positive_or_negative(i);
+
+        return (0);
+}

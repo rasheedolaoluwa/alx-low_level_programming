9c10c2993efaaebe58e154b9773abfa2094c0b5e
Rasheed Olaoluwa <radeyola@gmail.com>
2023-10-16 16:02:48 -0700

    try luck

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,47 +1,46 @@
 #include "main.h"
 
 /**
- * _atoi - Converts a string to an integer.
- * @s: The string to be converted.
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
  *
- * Return: The integer value of the converted string.
+ * Return: the int converted from the string
  */
 int _atoi(char *s)
 {
-	int i = 0, sign = 1, num = 0;
+	int i, d, n, len, f, digit;
 
-	/* Determine sign of the integer value */
-	while ((s[i] < '0' || s[i] > '9') && (s[i] != '\0'))
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len && f == 0)
 	{
 		if (s[i] == '-')
+			++d;
+
+		if (s[i] >= '0' && s[i] <= '9')
 		{
-			sign *= -1;
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
 		}
 		i++;
 	}
 
-	/* Calculate the integer value */
-	while ((s[i] >= '0') && (s[i] <= '9'))
-	{
-		num = (num * 10) + (s[i] - '0');
-		i++;
-	}
+	if (f == 0)
+		return (0);
 
-	return (num * sign);
-}
-
-/**
- * main - Entry point function.
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	char s[] = "   -9876543210abcd";
-	int number;
-
-	number = _atoi(s);
-	printf("%d\n", number);
-
-	return (0);
+	return (n);
 }
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 
 #include <stdio.h>
+#include <unistd.h>
 
 int _putchar(char c);
 void _puts(char *str);

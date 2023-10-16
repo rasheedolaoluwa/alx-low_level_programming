faca07dd1e0ef11c70001e47009d7e0a7457f909
Rasheed Olaoluwa <radeyola@gmail.com>
2023-10-16 15:02:43 -0700

    puts

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+void _puts(char *str);
 void swap_int(int *a, int *b);
 void reset_to_98(int *n);
 int _strlen(char *s);
+
+#endif /* MAIN_H */

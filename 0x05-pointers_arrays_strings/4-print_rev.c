e95ac2263e11b281d9ae73f40daedda6dc5603e2
Rasheed Olaoluwa <radeyola@gmail.com>
2023-10-16 15:13:25 -0700

    betty style update

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,20 +8,20 @@
  */
 void print_rev(char *s)
 {
-    int length = 0;
+	int length = 0;
 
-    /* find the length of the string */
-    while (s[length] != '\0')
-    {
-        length++;
-    }
+	/* find the length of the string */
+	while (s[length] != '\0')
+	{
+		length++;
+	}
 
-    /* start from the end of the string */
-    for (length = length - 1; length >= 0; length--)
-    {
-        _putchar(s[length]);
-    }
+	/* start from the end of the string */
+	for (length = length - 1; length >= 0; length--)
+	{
+		_putchar(s[length]);
+	}
 
-    /* print a new line */
-    _putchar('\n');
+	/* print a new line */
+	_putchar('\n');
 }

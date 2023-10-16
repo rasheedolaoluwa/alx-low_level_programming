0acb9680267121bdb7a3013a3af73a872198afe1
Rasheed Olaoluwa <radeyola@gmail.com>
2023-10-16 15:11:30 -0700

    print_rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_rev - Prints a string in reverse followed by a new line
+ * @s: The string to be printed
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+    int length = 0;
+
+    /* find the length of the string */
+    while (s[length] != '\0')
+    {
+        length++;
+    }
+
+    /* start from the end of the string */
+    for (length = length - 1; length >= 0; length--)
+    {
+        _putchar(s[length]);
+    }
+
+    /* print a new line */
+    _putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -6,5 +6,6 @@ void _puts(char *str);
 void swap_int(int *a, int *b);
 void reset_to_98(int *n);
 int _strlen(char *s);
+void print_rev(char *s);
 
 #endif /* MAIN_H */

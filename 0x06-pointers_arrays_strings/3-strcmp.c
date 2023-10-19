cacae2fc2c0a248b78bec1473f8ec8cba35770dc
Rasheed Olaoluwa <radeyola@gmail.com>
2023-10-19 15:12:49 -0700

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcmp - Compares two strings.
+ * @s1: The first string to be compared.
+ * @s2: The second string to be compared.
+ *
+ * Return: 
+ *   If s1 < s2, a negative value.
+ *   If s1 == s2, 0.
+ *   If s1 > s2, a positive value.
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && (*s1 == *s2))
+	{
+		s1++;
+		s2++;
+	}
+
+	return (*(unsigned char *)s1 - *(unsigned char *)s2);
+}

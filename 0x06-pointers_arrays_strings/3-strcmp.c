edd9666685a64b648e7fe97caac65a8bed1dce76
Rasheed Olaoluwa <radeyola@gmail.com>
2023-10-19 15:20:27 -0700

    betty update

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,11 +5,11 @@
  * @s1: input value
  * @s2: input value
  *
- * Description: This function compares the characters of two strings s1 and s2
- * character by character. If a discrepancy is found, it returns the ASCII
- * difference of the characters, else it returns 0 if the strings are identical.
+ * Description: Compares two strings character by character.
+ * If a discrepancy is found, returns the ASCII difference.
+ * Returns 0 if the strings are identical.
  *
- * Return: s1[i] - s2[i] if the strings are different, 0 if they are identical.
+ * Return: s1[i] - s2[i] if different, 0 if identical.
  */
 int _strcmp(char *s1, char *s2)
 {

8924c1431c9cb33700077b39bfebdaf8c6177944
Rasheed Olaoluwa <radeyola@gmail.com>
2023-10-25 17:10:55 -0700

    seond try

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,39 +1,28 @@
 #include "main.h"
 
 /**
- * check_match - Checks if two strings are identical considering wildcard.
- * @s1: The first string.
- * @s2: The second string.
- * @i: The index of the first string.
- * @j: The index of the second string.
+ * wildcmp - compares two strings, considering wildcards
+ * @s1: the primary string
+ * @s2: the string to compare, can contain wildcard '*'
  *
- * Return: 1 if the strings are identical, 0 otherwise.
- */
-int check_match(char *s1, char *s2, int i, int j)
-{
-	/* base case if we reach the end of both strings */
-	if (s1[i] == '\0' && s2[j] == '\0')
-		return (1);
-
-	/* if the characters match, or if s2 has a wildcard '*' */
-	if ((s1[i] == s2[j]) || (s2[j] == '*'))
-	{
-		if (s2[j] == '*' && s1[i] != '\0')
-			return (check_match(s1, s2, i + 1, j) || check_match(s1, s2, i, j + 1));
-		return (check_match(s1, s2, i + 1, j + 1));
-	}
-
-	return (0);
-}
-
-/**
- * wildcmp - Compares two strings considering wildcard.
- * @s1: The first string.
- * @s2: The second string.
- *
- * Return: 1 if the strings are identical, 0 otherwise.
+ * Return: 1 if considered identical, otherwise 0
  */
 int wildcmp(char *s1, char *s2)
 {
-	return (check_match(s1, s2, 0, 0));
+	if (!*s1)
+	{
+		while (*s2 == '*')
+			s2++;
+		return (!*s2);
+	}
+	if (*s2 == '*')
+	{
+		if (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1))
+			return (1);
+	}
+	else if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	return (0);
 }

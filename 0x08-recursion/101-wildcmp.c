1e3c2a8b78dbbd2dd1ae5f8f2c416e56a5dcd8dd
Rasheed Olaoluwa <radeyola@gmail.com>
2023-10-25 17:17:27 -0700

    third try

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,28 +1,25 @@
 #include "main.h"
 
 /**
- * wildcmp - compares two strings, considering wildcards
- * @s1: the primary string
- * @s2: the string to compare, can contain wildcard '*'
+ * wildcmp - compares two strings and returns 1 if the strings
+ * can be considered identical, otherwise return 0.
+ * @s1: the first string
+ * @s2: the second string which can contain the special character *
  *
- * Return: 1 if considered identical, otherwise 0
+ * Return: 1 if the strings are identical, otherwise return 0
  */
 int wildcmp(char *s1, char *s2)
 {
-	if (!*s1)
-	{
-		while (*s2 == '*')
-			s2++;
-		return (!*s2);
-	}
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
 	if (*s2 == '*')
 	{
-		if (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1))
+		if (*(s2 + 1) == '*')
+			return (wildcmp(s1, s2 + 1));
+		if (wildcmp(s1, s2 + 1) || (*s1 != '\0' && wildcmp(s1 + 1, s2)))
 			return (1);
-	}
-	else if (*s1 == *s2)
-	{
-		return (wildcmp(s1 + 1, s2 + 1));
 	}
 	return (0);
 }

6fd8651f2adc7830c293f86230a9f471c1a0440c
Rasheed Olaoluwa <radeyola@gmail.com>
2023-10-27 11:55:45 -0700

    udate mul checker work

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -3,23 +3,25 @@
 #include <stdlib.h>
 
 /**
- * main - Entry point, multiplies two numbers
- * @argc: the number of arguments
- * @argv: array of pointers to the arguments
+ * main - Entry point, multiplies two numbers.
+ * @argc: The number of arguments
+ * @argv: Array of pointers to the arguments
  *
- * Return: 0 (Success), or 1 if the program does not receive two arguments
+ * Return: 0 on success, or 1 if the program does not receive two arguments
  */
 int main(int argc, char *argv[])
 {
+	int num1, num2, product;
+
 	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
 
-	int num1 = atoi(argv[1]);
-	int num2 = atoi(argv[2]);
-	int product = num1 * num2;
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	product = num1 * num2;
 
 	printf("%d\n", product);
 

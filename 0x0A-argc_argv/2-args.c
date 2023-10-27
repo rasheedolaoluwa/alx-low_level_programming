c192725c5da9176ee0a716caefd31b677072fe62
Rasheed Olaoluwa <radeyola@gmail.com>
2023-10-27 11:41:44 -0700

    args 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point, prints all arguments it receives
+ * @argc: the number of arguments
+ * @argv: array of pointers to the arguments
+ *
+ * Return: 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+
+	return (0);
+}

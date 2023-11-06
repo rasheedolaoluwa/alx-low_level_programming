fb7f589c9b8866a18625dde482b02f921b7d5156
Rasheed Olaoluwa <radeyola@gmail.com>
2023-11-06 14:45:31 -0800

    update dog header

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -23,5 +23,8 @@ typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
+dog_t *new_dog(char *name, float age, char *owner);
 
 #endif /* DOG_H */

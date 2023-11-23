0d4f186b2b41467186aae05be86a4f4ffacae477
rasheed <radeyola@gmail.com>
2023-11-23 04:11:53 -0800

    uint 2

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -21,7 +21,8 @@ unsigned int binary_to_uint(const char *b)
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
 
-		num = num * 2 + (b[i] - '0');
+		num <<= 1;
+		num += b[i] - '0';
 	}
 
 	return (num);

3e01969f12e61b8013664da91fcb5fad82cc101f
rasheed <radeyola@gmail.com>
2023-11-27 16:27:38 -0800

    semantic

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -6,42 +6,42 @@
 #include <stdlib.h>
 
 /**
- * read_text_file - reads a text file and prints to POSIX stdout
- * @file_path: path to the file to read
- * @num_chars: number of characters it should read and print
+ * read_textfile - reads a text file and prints it to POSIX stdout
+ * @filename: the name of the file to read
+ * @letters: the number of characters to read and print
  *
  * Return: the actual number of characters it could read and print
  */
-ssize_t read_text_file(const char *file_path, size_t num_chars)
+ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
 	ssize_t read_count, write_count;
-	char *read_buffer;
+	char *buffer;
 
-	if (file_path == NULL)
+	if (filename == NULL)
 		return (0);
 
-	fd = open(file_path, O_RDONLY);
+	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
 
-	read_buffer = malloc(sizeof(char) * num_chars);
-	if (read_buffer == NULL)
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
 	{
 		close(fd);
 		return (0);
 	}
 
-	read_count = read(fd, read_buffer, num_chars);
+	read_count = read(fd, buffer, letters);
 	close(fd);
 	if (read_count == -1)
 	{
-		free(read_buffer);
+		free(buffer);
 		return (0);
 	}
 
-	write_count = write(STDOUT_FILENO, read_buffer, read_count);
-	free(read_buffer);
+	write_count = write(STDOUT_FILENO, buffer, read_count);
+	free(buffer);
 	if (read_count != write_count)
 		return (0);
 

1260f68e50130af01c8a0abf59f2c2f0bfcf265c
rasheed <radeyola@gmail.com>
2023-11-27 16:22:36 -0800

    update

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -3,10 +3,10 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
@@ -14,4 +14,4 @@ int create_file(const char *filename, char *text_content);
 int _strlen(char *str);
 int append_text_to_file(const char *filename, char *text_content);
 
-#endif
+#endif /* MAIN_H */

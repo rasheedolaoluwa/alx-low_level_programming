e88d5cb49d141d1d76b47b2aaa4519db8df4c6fa
rasheed <radeyola@gmail.com>
2024-01-15 14:33:22 -0800

    memset

--- 0x18-dynamic_libraries/0-isupper.c
+++ 0x18-dynamic_libraries/0-isupper.c
@@ -1,12 +1,12 @@
 #include "main.h"
-#include <studio.h>
+#include <stdio.h>
 /**
  * _isupper - check if c is uppercase
  *
  * @c: input for character
  *
- * Retrun: 1 if its uppercase, o if not
- */
+ * Return: 1 if its uppercase, 0 if not
+*/
 
 int _isupper(int c)
 {
